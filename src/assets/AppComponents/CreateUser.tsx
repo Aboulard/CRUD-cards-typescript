4f441bc37fc91a3434ec9aec4a2db4c77d92cbb3
Aboulard <antoninboulard1998@gmail.com>
2024-01-15 11:55:05 +0100

    [RENAME] renaming file for clarity

---
+++ src/assets/AppComponents/CreateUser.tsx
@@ -0,0 +1,87 @@
+import Button from '@mui/material/Button';
+import { TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { User } from '../../App';
+import { useState } from 'react';
+
+export type CreateUserType = {
+    setUsersArray: React.Dispatch<React.SetStateAction<User[]>>;
+    numberElem: number;
+    setnumberElem: React.Dispatch<React.SetStateAction<number>>;
+  }
+  
+
+export function MyCreateUser( {setUsersArray, numberElem, setnumberElem} : CreateUserType) {
+    /*
+    Problème pas grave mais problème quand même:
+    Quand je modifie les infos pour la création d'un User, c'est sauvegardé, ça se reset pas
+    Peut-être rajouter un truc qui empeche d'entrer des infos vides    
+    */
+
+    const [openCreate, setOpenCreate] = useState(false);
+    let [tmp_user, settmp_user] = useState<User>({ ID: -1, Nom: "tmp", Age: 0, Prénom: "tmp", Photo: "tmp" })
+
+    const handleClickCreateOpen = () => {
+        setOpenCreate(true);
+    };
+
+    const handleCreateClose = () => {
+        setOpenCreate(false);
+    };
+
+    const CreateUser = (my_user: User) => {
+        setUsersArray(usersArray => [...usersArray, { ID: numberElem, Nom: my_user.Nom, Age: my_user.Age, Prénom: my_user.Prénom, Photo: my_user.Photo }])
+        setnumberElem(numberElem + 1)
+    }
+
+    return (
+        <>
+        <Button
+            sx={{ margin: 'auto' }}
+            variant='contained'
+            onClick={handleClickCreateOpen}
+        >
+            Add User
+        </Button>
+        <Dialog open={openCreate} onClose={handleCreateClose} title='Dialog Box to Create new User'>
+                <DialogTitle>Create User</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Insert information corresponding to the User you are trying to add
+                    </DialogContentText>
+                    <TextField
+                        //                    required
+                        id="Prénom"
+                        label="Prénom"
+                        variant="outlined"
+                        margin="normal"
+                        onChange={(v) => settmp_user((previous: User) => ({ ...previous, Prénom: v.target.value }))} /><br></br>
+                    <TextField
+                        //                    required
+                        id="Nom"
+                        label="Nom"
+                        variant="outlined"
+                        margin="normal"
+                        onChange={(v) => settmp_user((previous: User) => ({ ...previous, Nom: v.target.value }))} /><br></br>
+                    <TextField
+                        //                    required
+                        id="Age"
+                        label="Age"
+                        type="number"
+                        variant="outlined"
+                        margin="normal"
+                        onChange={(v) => settmp_user((previous: User) => ({ ...previous, Age: Number(v.target.value) }))} /><br></br>
+                    <TextField
+                        //                    required
+                        id="Photo_URL"
+                        label="Photo_URL"
+                        variant="outlined"
+                        margin="normal"
+                        onChange={(v) => settmp_user((previous: User) => ({ ...previous, Photo: v.target.value }))} />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCreateClose}>Cancel</Button>
+                    <Button onClick={() => { CreateUser(tmp_user); handleCreateClose(); } }>Add User</Button>
+                </DialogActions>
+            </Dialog></>
+    )
+}
